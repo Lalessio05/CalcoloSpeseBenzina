e4e07883d7c8fe43546c8cb712a035243f8f1597
Alessio Catrini <catrini.alessio2@gmail.com>
2024-06-26 01:32:22 +0200

    Iniziata struttura crawling principale maps

---
+++ CalcoloSpeseBenzina/Components/Percorso.cs
@@ -0,0 +1,8 @@
+﻿namespace CalcoloSpeseBenzina.Components
+{
+    public class Percorso(string partenza = "", string arrivo = "")
+    {
+        public string Partenza { get; set; } = partenza;
+        public string Arrivo { get; set; } = arrivo;
+    }
+}
--- CalcoloSpeseBenzina/Crawler.cs
+++ CalcoloSpeseBenzina/Crawler.cs
@@ -1,3 +1,4 @@
+using CalcoloSpeseBenzina.Components;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -5,7 +6,7 @@ using SeleniumExtras.WaitHelpers;
 
 namespace CalcoloSpeseBenzina;
 
-internal static class Crawler
+internal static class Crawler   //Va fatto diventare un singleton
 {
     public static (double benzina, double gasolio) CheckPrices()
     {
@@ -20,8 +21,27 @@ internal static class Crawler
     private static (double, double) GetPrices(IWebElement row)
     {
         var cells = row.FindElements(By.TagName("td")).Select(x => x.Text.Replace('.', ',')).ToList();
-        double benzina = double.Parse(cells[2]);
-        double diesel = double.Parse(cells[1]);
+        var benzina = double.Parse(cells[2]);
+        var diesel = double.Parse(cells[1]);
         return (benzina, diesel);
+    }
+
+    public static void StartCrawling(List<Percorso> percorsi)
+    {
+        using WebDriver crawler = new ChromeDriver();
+        WebDriverWait wait = new(crawler, TimeSpan.FromSeconds(10));
+        crawler.Navigate().GoToUrl("https://www.google.com/maps");
+        var cookiesButton = crawler.FindElement(By.XPath("(.//span[text()='Accetta tutto'])[1]"));
+        wait.Until(_=>cookiesButton.Enabled);
+        cookiesButton.Click();
+        var searchBar = crawler.FindElement(By.XPath(".//input[1]"));
+        wait.Until(_ => searchBar.Enabled);
+        foreach (var percorso in percorsi)
+        {
+            searchBar.SendKeys(percorso.Partenza);
+            var result = crawler.FindElement(By.XPath(".//input[1]/../../following-sibling::*[1]"));
+            wait.Until(_ => result.Enabled);
+            result.Click();
+        }
     }
 }
